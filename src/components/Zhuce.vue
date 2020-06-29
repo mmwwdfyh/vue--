324bdcaee8c18cd5b6be5640f1119336472f7edf
fyh.1314 <474038646@qq.com>
2020-06-29 20:45:36 +0800

    注册验证

---
+++ src/components/Zhuce.vue
@@ -0,0 +1,174 @@
+<template>
+  <div>
+    <van-nav-bar title="标题" left-text="返回"  />
+    <!-- <van-cell-group> -->
+    <van-form>
+      <van-field v-model="mobile" type="tel" label="手机号" right-icon="graphic" />
+      <van-field
+        v-model="pwd"
+        :type="hidePwd?'password':'text'"
+        :right-icon="hidePwd?'closed-eye':'eye-o'"
+        @click-right-icon="hidePwd = !hidePwd"
+        label="密码"
+      />
+      <van-field
+        v-model="pwd1"
+        :type="hidePwd1?'password':'text'"
+        :right-icon="hidePwd1?'closed-eye':'eye-o'"
+        @click-right-icon="hidePwd1 = !hidePwd1"
+        label="确认密码"
+      />
+      <van-field placeholder="用户名" class="inp" v-model="nick" right-icon="user-o" />
+      <van-field v-model="picCode" label="图形验证">
+        <template #right-icon>
+          <van-image :src="imgUrl" @click="createImgCode" />
+        </template>
+      </van-field>
+      <!-- 城市信息 -->
+      <van-field placeholder="城市" v-model="area" right-icon="location-o" @click="areaShow=true" />
+      <!-- 城市列表，底部弹出层 -->
+      <van-popup v-model="areaShow" position="bottom">
+        <van-area
+          title="选择城市"
+          :area-list="aresList"
+          @cancel="areaShow=false"
+          @confirm="selectedArea"
+        />
+        <!-- 关闭弹框和完成 -->
+      </van-popup>
+
+      <van-field placeholder="手机验证码" v-model="code">
+        <template #button>
+          <van-button
+            type="danger"
+            plain
+            size="small"
+            :disabled="btnIsDisabled"
+            @click="sendCode"
+          >{{msg}}</van-button>
+        </template>
+      </van-field>
+      <van-button type="info" block @click="checkForm">立即注册</van-button>
+      <!-- </van-cell-group> -->
+    </van-form>
+  </div>
+</template>
+
+<script>
+import AreaList from "@/assets/area";
+export default {
+    name:"",
+    mounted(){
+        this.createImgCode();
+    },
+  data() {
+    return {
+      mobile: "",
+      pwd: "",
+      nick: "",
+      pwd1: "",
+      picCode: "",
+      code: "",
+      hidePwd: true,
+      hidePwd1: true,
+      imgUrl: "",
+      picKey: "",
+      //选择城市信息
+      areaShow: false,
+      aresList: AreaList, //城市列表数据
+      area: "",
+      province: "",
+      city: "",
+      //倒计时的功能
+      msg: "发送验证码",
+      btnIsDisabled: false
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    //基本的表单校验
+    checkForm() {
+      if (this.mobile == "") {
+        this.$toast.fail("手机号不能为空"); // vant样式
+        return false;
+      }
+      //校验手机号的格式是否合法
+      var reg = /^1[345678]\d{9}$/;
+      if (!reg.test(this.mobile)) {
+        this.$toast.fail("请输入正确的手机号");
+        return false;
+      }
+      //确认密码和密码是否一致
+      if (this.pwd1 != this.pwd) {
+        this.$toast.fail("密码不一致，请重新输入");
+        return false;
+      }
+      this.$toast.success("恭喜你,注册成功");
+    },
+    //生成图形验证码
+    createImgCode() {
+        this.picKey = (new Date()).getTime();
+        let apiUrl = "https://api.it120.cc/small4/verification/pic/get";
+        this.imgUrl = `${apiUrl}?key=${this.picKey}`;
+    },
+    //选择城市函数
+    selectedArea(arr){
+        this.areaShow = false;
+         //去除数组中的name值
+         let tmp = arr.map((item)=>{
+             return item.name;
+         });
+           //处理省份和市
+           this.province = tmp[0];
+           this.city = tmp[1];
+            //数组转换程字符串
+      this.area = tmp.join(" ");
+    },
+        //倒计时的功能
+    countSeconds(){
+      this.btnIsDisabled = true;
+      //倒计时的时间
+      let timeout = 60;
+      //倒计时间歇
+      let timer = setInterval(() => {
+        //读秒结束
+        if(timeout<1){
+          this.btnIsDisabled =false;
+          //清除定时器
+          clearInterval(timer);
+          this.msg = `重新发送`;
+          return false;
+        }
+        //正在倒计时的时候
+        this.msg = `${timeout}s后再试`
+        timeout--;
+      }, 1000);
+    },
+     //发送验证码接口
+     sendCode(){
+         this.$axios({
+             url:"https://api.it120.cc/small4/verification/sms/get",
+             params:{
+                 mobile:this.mobile,
+                 key:this.Key,
+                 picCode:this.picCode,
+             }
+         }).then((res)=>{
+             if(res.code !=0){
+                 this.$toast.fail(res.msg);
+                 return false;
+             }
+             this.countSeconds();
+             console.log(res)
+         })
+     }
+  }
+};
+</script>
+
+<style scoped lang="less">
+.content {
+  padding: 16px 16px 160px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -28,14 +28,15 @@ axios.interceptors.request.use(
 // 响应
 axios.interceptors.response.use(
   response => {
-    // console.log(response)
+    console.log(response)
     // store.state.isLoading = false
     // 如果返回的状态码为200，说明接口请求成功，可以正常拿到数据
     // 否则的话抛出错误
     if (response.status === 200) {
-      return Promise.resolve(response);
+      return Promise.resolve(response.data);
     } else {
       return Promise.reject(response);
+      
     }
   },
   // 服务器状态码不是2开头的的情况
